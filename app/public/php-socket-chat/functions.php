30d5a27ba3c6c941942b0c158b834c09452f0507
Abhinav R <abhinav7r@gmail.com>
2022-08-12 18:42:12 +0530

    edited socket functions.php

--- app/public/php-socket-chat/functions.php
+++ app/public/php-socket-chat/functions.php
@@ -2,17 +2,17 @@
 
 function unmask($text) {
     $length = @ord($text[1]) & 127;
-    if($length == 126) {    
+    if($length == 126) {
 		$masks = substr($text, 4, 4);    $data = substr($text, 8); 
 	}
-    elseif($length == 127) {    
+    elseif($length == 127) {
 		$masks = substr($text, 10, 4); $data = substr($text, 14); 
 	}
-    else { 
+    else {
 		$masks = substr($text, 2, 4); $data = substr($text, 6); 
 	}
     $text = "";
-    for ($i = 0; $i < strlen($data); ++$i) { 
+    for ($i = 0; $i < strlen($data); ++$i) {
 		$text .= $data[$i] ^ $masks[$i % 4];    
 	}
     return $text;
@@ -43,8 +43,7 @@ function handshake($request_header,$sock, $host_name, $port) {
 	foreach($lines as $line)
 	{
 		$line = chop($line);
-		if(preg_match('/\A(\S+): (.*)\z/', $line, $matches))
-		{
+		if(preg_match('/\A(\S+): (.*)\z/', $line, $matches)){
 			$headers[$matches[1]] = $matches[2];
 		}
 	}
